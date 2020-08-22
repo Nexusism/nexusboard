66ded2dc4a3fb39a44b1e5c4450900817c957e54
Nexusism <wanggoog91@gmail.com>
2020-08-22 14:16:38 +0900

    게시판 수정 삭제까지

--- src/main/java/com/nexus/board/controller/BoardController.java
+++ src/main/java/com/nexus/board/controller/BoardController.java
@@ -4,8 +4,10 @@ import com.nexus.board.dto.BoardDto;
 import com.nexus.board.service.BoardService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -13,13 +15,45 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/")
-    public String list() {
+    public String list(Model model) {
+        List<BoardDto> boardList = boardService.getBoardlist();
+        model.addAttribute("boardList", boardList);
         return "board/list.html";
     }
 
     @GetMapping("/post")
     public String write() {
         return "board/write.html";
+    }
+
+    @GetMapping("/post/{no}")
+    public String detail(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDTO = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDTO);
+        return "board/detail.html";
+    }
+
+    @GetMapping("/post/edit/{no}")
+    public String edit(@PathVariable("no") Long no, Model model) {
+        BoardDto boardDTO = boardService.getPost(no);
+
+        model.addAttribute("boardDto", boardDTO);
+        return "board/update.html";
+    }
+
+    @PutMapping("/post/edit/{no}")
+    public String update(BoardDto boardDTO) {
+        boardService.savePost(boardDTO);
+
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/post/{no}")
+    public String delete(@PathVariable("no") Long no) {
+        boardService.deletePost(no);
+
+        return "redirect:/";
     }
 
     @PostMapping("/post")
--- src/main/java/com/nexus/board/service/BoardService.java
+++ src/main/java/com/nexus/board/service/BoardService.java
@@ -1,11 +1,15 @@
 package com.nexus.board.service;
 
+import com.nexus.board.domain.entity.BoardEntity;
 import com.nexus.board.domain.repository.BoardRepository;
 import com.nexus.board.dto.BoardDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -13,7 +17,48 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
+    public List<BoardDto> getBoardlist() {
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for ( BoardEntity boardEntity : boardEntities) {
+            BoardDto boardDTO = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+
+            boardDtoList.add(boardDTO);
+        }
+
+        return boardDtoList;
+    }
+
+    @Transactional
+    public BoardDto getPost(Long id) {
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDTO = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+
+        return boardDTO;
+    }
+
+    @Transactional
     public Long savePost(BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public void deletePost(Long id) {
+        boardRepository.deleteById(id);
     }
 }
